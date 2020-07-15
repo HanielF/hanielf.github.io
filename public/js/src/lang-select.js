3c0b8787ec17bf2050965f428d33c9aecc99bd15
hanielf <hanielxx@outlook.com>
2020-07-15 17:10:08 +0800

    Add public files

---
+++ public/js/src/lang-select.js
@@ -0,0 +1,3 @@
+// build time:Wed Jul 15 2020 16:06:06 GMT+0800 (China Standard Time)
+(function(){"use strict";function e(){var e=this.value;var t=this.dataset.canonical;if(e==="en")e="";if(e)e+="/";location.href="/"+e+t}document.getElementById("lang-select").addEventListener("change",e);document.getElementById("mobile-lang-select").addEventListener("change",e)})();
+//rebuild by neat 
