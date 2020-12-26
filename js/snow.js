578eaf2e1fb266c9a86760c5411126edfea44675
hanielf <hanielxx@outlook.com>
2020-12-26 17:16:43 +0800

    Site updated: 2020-12-26 17:16:42

--- js/snow.js
+++ js/snow.js
@@ -87,7 +87,7 @@ function snowCanvas() {
     /* 窗口大小改变的处理 */
     window.onresize = function() {
         snowcanvas.width = window.innerWidth;
-        // snowcanvas.height = window.innerHeight;
+        snowcanvas.height = document.body.scrollHeight;
     }
 }
 /* 雪运动对象 */
